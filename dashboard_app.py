dd397d5d7e17575cf6c46c11e796dc07cbfd73ca
leonirscatolin <jrscatolin@hotmail.com>
2025-10-21 13:18:31 -0300

    Update dashboard_app.py

--- dashboard_app.py
+++ dashboard_app.py
@@ -439,7 +439,7 @@ try:
                         category_orders={'Atribuir a um grupo': group_totals.index.tolist(), 'Faixa de Antiguidade': ordem_faixas},
                         color_discrete_map=color_map, text_auto=True
                     )
-                    fig_stacked_bar.update_traces(textangle=0, textfont_size=12)
+                    fig_stacked_bar.update_traces(textangle=0, textfont_size=12, textposition='auto')
                     fig_stacked_bar.update_layout(height=dynamic_height, yaxis={'categoryorder':'total ascending'}, legend_title_text='Antiguidade')
                 
                 else: # Vertical
@@ -450,7 +450,7 @@ try:
                         category_orders={'Atribuir a um grupo': group_totals.index.tolist(), 'Faixa de Antiguidade': ordem_faixas},
                         color_discrete_map=color_map, text_auto=True
                     )
-                    fig_stacked_bar.update_traces(textangle=0, textfont_size=12)
+                    fig_stacked_bar.update_traces(textangle=0, textfont_size=12, textposition='auto')
                     fig_stacked_bar.update_layout(height=600, xaxis_title=None, xaxis_tickangle=-45, legend_title_text='Antiguidade')
 
                 st.plotly_chart(fig_stacked_bar, use_container_width=True)
